11f0ce531d39cd86c188801ca73d9a64a40e1c19
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-06-24 20:00:31 +0200

    Refactored code for readability and style compliance

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,18 +1,19 @@
 def caesar_cipher(unencrypted_text, shift)
-  encrypted_text = ""
-  alphabet = ("a".."z").to_a
+  encrypted_text = ''
+  alphabet = ('a'..'z').to_a
 
   unencrypted_text.each_char do |letter|
-    if alphabet.include?(letter.downcase)
-      index = alphabet.index(letter.downcase)
-      puts "#{index}: #{letter}"
-      shifted_index = (index + shift) % alphabet.length
-      shifted_letter = alphabet[shifted_index]
-      puts "#{shifted_index}: #{shifted_letter}" 
-      encrypted_text += shifted_letter.to_s
-    end
-  end 
+    next unless alphabet.include?(letter.downcase)
+
+    index = alphabet.index(letter.downcase)
+    puts "#{index}: #{letter}"
+    shifted_index = (index + shift) % alphabet.length
+    shifted_letter = alphabet[shifted_index]
+    puts "#{shifted_index}: #{shifted_letter}"
+    encrypted_text += shifted_letter.to_s
+  end
   encrypted_text
 end
-encrypted_text = caesar_cipher("Hello World!", 7)
+
+encrypted_text = caesar_cipher('Hello World!', 7)
 puts encrypted_text
